1b24119e0f8b1f1c7b0463ae5f8b161bded48884
PedroJoMa <pejomadejefi@gmail.com>
2023-09-04 15:34:42 -0300

    feat: chaves de github e website

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -11,14 +11,16 @@ function Projects () {
       title: 'Jogo de Trivia',
       description: 'O objetivo deste projeto foi criar um jogo de perguntas e respostas utilizando uma API de trivia, tive a experiência de trabalhar em grupo com 4 colegas, foi muito divertido codar e dividir tarefas.',
       tecnologies: [imagens.React, imagens.JavaScript, imagens.Css, imagens.Html, imagens.Redux, imagens.Api],
-      link: "https://github.com/PedroJoMa/JogoDeTrivia",
+      github: "https://github.com/PedroJoMa/JogoDeTrivia",
+      website: "https://jogodetriviadevpedrojorge.vercel.app/",
     },
     {
       imageUrl: imagens.AppReceitas,
       title: 'App de Receitas',
       description:"O projeto app de receitas é uma aplicação voltada para o mobile a qual o usuário tem acesso a diversas receitas de comidas e bebidas, ao selecionar uma receita, são fornecidos: ingredientes necessários e modo de preparo. Realizei este trabalho em grupo com outros 4 colegas e o resultado foi muito satisfatório.",
       tecnologies: [imagens.React, imagens.JavaScript, imagens.Jest, imagens.Html, imagens.Css, imagens.Api],
-      link: "https://github.com/PedroJoMa/AppDeReceitas",
+      github: "https://github.com/PedroJoMa/AppDeReceitas",
+      website: "inserir",
     }
   ]
 
